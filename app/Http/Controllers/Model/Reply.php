215810d6c36a1a35b89fb4a9d019aa927eb4c7e4
maruf786 <maruf@modello.my>
2021-02-06 18:44:52 +0600

    Creating Migrations, Models, Controllers, Factories

---
+++ app/Http/Controllers/Model/Reply.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Model;
+
+use App\fr;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class Reply extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\fr  $fr
+     * @return \Illuminate\Http\Response
+     */
+    public function show(fr $fr)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\fr  $fr
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(fr $fr)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\fr  $fr
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, fr $fr)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\fr  $fr
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(fr $fr)
+    {
+        //
+    }
+}
