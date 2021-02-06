07ef94256a554df5bbb9dff4afeadd294f636d91
maruf786 <maruf@modello.my>
2021-02-06 23:58:36 +0600

    Creating Relationship among models

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -2,9 +2,26 @@
 
 namespace App\Model;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $fillables = ['title','slug','body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
 }
